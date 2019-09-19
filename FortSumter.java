3a04ba9480f1d66de9532c893a22d65ded499d8f
Ethan Lau <Lau91841>
2019-09-19 09:37:45 -0700

    test

--- FortSumter.java
+++ FortSumter.java
@@ -12,6 +12,11 @@ public class FortSumter extends Ukraine
      * Act - do whatever the FortSumter wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
      */
+    private GreenfootImage image2;
+    public void FortSumter() {
+        image2 = new GreenfootImage("sumter.jpg");
+        setImage(image2);
+    }
     public void act() 
     {
         // Add your action code here.
